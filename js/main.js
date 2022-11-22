ee3a8ee765639d95b23b90d49a3cb0f9eb3d3a23
Riabov Vladyslav <project5@sviteco.com>
2022-11-22 16:22:20 +0200

    refactor: move player to own file

--- js/main.js
+++ js/main.js
@@ -14,41 +14,6 @@ const keys = {
   },
 };
 //ser player class
-class Player {
-  constructor() {
-    this.position = {
-      x: 200,
-      y: 0,
-    };
-    this.velocity = {
-      x: 0,
-      y: 0,
-    };
-    this.height = 100;
-    this.width = 100;
-  }
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-  gravity() {
-    this.position.y += this.velocity.y;
-    if (this.position.y + this.height + this.velocity.y < cHeigth)
-      this.velocity.y += gravity;
-    else this.velocity.y = 0;
-  }
-  move() {
-    this.velocity.x = 0
-    if(keys.d.pressed)this.velocity.x = 5
-    else if(keys.a.pressed)this.velocity.x = -5
-    this.position.x += this.velocity.x;
-  }
-  update() {
-    this.gravity();
-    this.move();
-    this.draw();
-  }
-}
 
 const player = new Player();
 
