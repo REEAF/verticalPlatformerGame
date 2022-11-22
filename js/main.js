d6b1374329c887f399476b79e09fff1db8980e74
Riabov Vladyslav <project5@sviteco.com>
2022-11-22 16:56:41 +0200

    feat: add background

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 const $canvas = document.querySelector("canvas");
 const c = $canvas.getContext("2d");
+
+const player = new Player();
+const background = new Sprite({
+  position: { x: 0, y: 0 },
+  imageSrc: "./img/background.png",
+});
+
+const gravity = 0.5;
+const scaleFactor = 4;
+
 //set canvas size
 cWidth = $canvas.width = 1024;
 cHeigth = $canvas.height = 576;
+const scaledCanvas = {
+  width: cWidth / scaleFactor,
+  height: cHeigth / scaleFactor,
+};
 
-const gravity = 0.5;
 const keys = {
   a: {
     pressed: false,
@@ -13,19 +26,24 @@ const keys = {
     pressed: false,
   },
 };
-//ser player class
-
-const player = new Player();
 
 function animation() {
   window.requestAnimationFrame(animation);
   //clear canvas
   c.fillStyle = "#333333";
   c.fillRect(0, 0, cWidth, cHeigth);
+  //setup background
+  c.save();
+  c.scale(4, 4);
+  c.translate(0, -background.image.height + scaledCanvas.height);
+  background.update();
+  c.restore();
+
   player.update();
 }
 
 animation();
+
 window.addEventListener("keydown", (e) => {
   switch (e.code) {
     case "KeyA":
@@ -37,7 +55,7 @@ window.addEventListener("keydown", (e) => {
     case "KeyW":
       player.velocity.y = -15;
       break;
-      case "Space":
+    case "Space":
       player.velocity.y = -15;
       break;
   }
